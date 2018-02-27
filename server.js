bbfc548bed39085be16b25ceeb8b94f60d527f12
MMehelich <cassiel8198@gmail.com>
2018-02-26 16:02:34 -0800

    First server ommit

---
+++ server.js
@@ -0,0 +1,18 @@
+'use strict'
+
+const express = require('express');
+const cors = require('cors');
+const pg = require('pg');
+
+const app = express();
+const PORT = process.env.PORT;
+const CLIENT_URL = process.env.CLIENT_URL;
+
+const client = new pg.client(process.env.DATABASE_URL);
+client.connect();
+
+app.use(cors());
+
+app.get('/', (reg, res) => res.send('Testing 1, 2, 3. A, B, C. Doh, Rai, Me.'));
+
+app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
