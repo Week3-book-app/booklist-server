93313e5ace2c241bf0cfdc995a19c4e714f9a9b2
haronYunis <haronyunis@icloud.com>
2018-02-28 12:16:02 -0800

    fixed bugs and added body-parser

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 const express = require('express');
 const cors = require('cors');
 const pg = require('pg');
+const bodyParser = require('body-parser');
 
 const app = express();
 const PORT = process.env.PORT;
@@ -20,15 +21,16 @@ app.get('/api/v1/books', (req, res) => {
   client.query(`SELECT * from books;`)
     .then(results => res.send(results.rows))
     .catch(console.error);
+});
 
-app.get('*', (req,res) => res.redirect(CLIENT_URL));
+app.get('*', (req, res) => res.redirect(CLIENT_URL));
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
 
-app.post('/books/add', bodyParser, (req, res) =>{
-  let {title, author, isbn, image_url, description} = req.body;
+app.post('/books/add', bodyParser, (req, res) => {
+  let { title, author, isbn, image_url, description } = req.body;
   //destructuring ^
 
-client.query(`
+  client.query(`
   INSERT INTO books(title, author, isbn, image_url, description) VALUES($1, $2, $3, $4, $5);`, [title, author, isbn, image_url, description])
     .then(() => res.sendStatus(201))
     .catch(errorCallback);
