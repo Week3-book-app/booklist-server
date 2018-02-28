cd2f9d068f5e3588743dc6d4d6a883ffb1f08bfa
haronYunis <haronyunis@icloud.com>
2018-02-28 12:06:39 -0800

    added event listener for error listening

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const CLIENT_URL = process.env.CLIENT_URL;
 const client = new pg.Client(process.env.DATABASE_URL);
 client.connect();
 
+client.on('error', err => console.error(err));
 app.use(cors());
 
 app.get('/', (req, res) => res.send('Testing: 1, 2, 3. A, B, C. Doh, Rai, Me.'));
