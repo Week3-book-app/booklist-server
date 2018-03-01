823559b04410d43620f09d5c9d33f088685522e7
MMehelich <cassiel8198@gmail.com>
2018-03-01 08:57:44 -0800

    trying to debug. Need help.

--- server.js
+++ server.js
@@ -15,23 +15,31 @@ app.use(cors());
 
 app.get('/', (req, res) => res.send('Testing: 1, 2, 3. A, B, C. Doh, Rai, Me.'));
 
+
+
 app.get('/api/v1/books', (req, res) => {
-  client.query(`SELECT * from books;`)
+  client.query(`SELECT id, title, author,image_url, isbn FROM books;`)
     .then(results => res.send(results.rows))
     .catch(console.error);
+});
 
-app.get('*', (req,res) => res.redirect(CLIENT_URL));
-app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
+app.get('/api/v1/books/:id', (req, res) => {
+  client.query(`SELECT * FROM books WHERE id=${req.params.id}`)
+    .then(results => res.send(results.rows))
+    .catch(console.error)
+});
+
 
 app.post('/books/add', bodyParser, (req, res) =>{
   let {title, author, isbn, image_url, description} = req.body;
   //destructuring ^
 
-client.query(`
+  client.query(`
   INSERT INTO books(title, author, isbn, image_url, description) VALUES($1, $2, $3, $4, $5);`, [title, author, isbn, image_url, description])
-    .then(() => res.sendStatus(201))
+    .then(results => res.sendStatus(201))
     .catch(errorCallback);
-
 });
 
+app.get('*', (req,res) => res.redirect(CLIENT_URL));
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
+
