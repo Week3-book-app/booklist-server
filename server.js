772f6b397ad8c37d90c59d6c59ea3c0072220a78
MMehelich <cassiel8198@gmail.com>
2018-03-01 11:16:52 -0800

    Debugged for book add form to work

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 const express = require('express');
 const cors = require('cors');
 const pg = require('pg');
+const bodyParser = require('body-parser');
 
 const app = express();
 const PORT = process.env.PORT;
@@ -13,8 +14,7 @@ client.connect();
 
 app.use(cors());
 
-app.get('/', (req, res) => res.send('Testing: 1, 2, 3. A, B, C. Doh, Rai, Me.'));
-
+// app.get('/', (req, res) => res.send('Testing: 1, 2, 3. A, B, C. Doh, Rai, Me.'));
 
 
 app.get('/api/v1/books', (req, res) => {
@@ -23,23 +23,23 @@ app.get('/api/v1/books', (req, res) => {
     .catch(console.error);
 });
 
-app.get('/api/v1/books/:id', (req, res) => {
-  client.query(`SELECT * FROM books WHERE id=${req.params.id}`)
+app.get('/api/v1/book/:id', (req, res) => {
+  client.query(`SELECT * FROM books WHERE id=${req.params.id};`)
     .then(results => res.send(results.rows))
     .catch(console.error)
 });
 
 
-app.post('/books/add', bodyParser, (req, res) =>{
+app.post('/book/add', bodyParser, (req, res) =>{
   let {title, author, isbn, image_url, description} = req.body;
   //destructuring ^
 
   client.query(`
   INSERT INTO books(title, author, isbn, image_url, description) VALUES($1, $2, $3, $4, $5);`, [title, author, isbn, image_url, description])
-    .then(results => res.sendStatus(201))
-    .catch(errorCallback);
+    .then(() => res.sendStatus(201))
+    .catch(app.errorCallback);
 });
 
 app.get('*', (req,res) => res.redirect(CLIENT_URL));
-app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
 
