a6863257123047b32660acee74d36db47a567ba5
MMehelich <cassiel8198@gmail.com>
2018-03-04 17:48:55 -0800

    Not complete but I tried.

--- server.js
+++ server.js
@@ -12,9 +12,7 @@ const CLIENT_URL = process.env.CLIENT_URL;
 const client = new pg.Client(process.env.DATABASE_URL);
 client.connect();
 
-app.use(cors({
-  origin: true,
-}));
+app.use(cors());
 
 app.get('/api/v1/books', (req, res) => {
   client.query(`SELECT * FROM books;`)
@@ -29,15 +27,34 @@ app.get('/api/v1/books/:id', (req, res) => {
 });
 
 
-app.post('/books/add', bodyParser, (req, res) =>{
+app.post('api/v1/books/add', bodyParser, (req, res) =>{
   let {title, author, isbn, image_url, description} = req.body;
-
   //destructuring ^
 
   client.query(`
-  INSERT INTO books(title, author, isbn, image_url, description) VALUES($1, $2, $3, $4, $5);`, [title, author, isbn, image_url, description])
-    .then(() => res.sendStatus(201))
-    .catch(app.errorCallback);
+    INSERT INTO books(title, author, isbn, image_url, description) VALUES($1, $2, $3, $4, $5)`,
+  [title, author, isbn, image_url, description]
+  )
+    .then(results => res.sendStatus(201))
+    .catch(console.error);
+});
+
+app.put('/api/v1/books/:id', bodyParser, (req, res) => {
+  let {title, author, isbn, image_url, description} = req.body;
+  client.query(`
+    UPDATE books
+    SET title=$1, author=$2, isbn=$3, image_url=$4, description=$5
+    WHERE id=$6`,
+  [title, author, isbn, image_url, description, req.params.id]
+  )
+    .then(() => res.sendStatus(204))
+    .catch(console.error)
+})
+
+app.delete('/api/v1/books/:id', (req, res) => {
+  client.query('DELETE FROM books WHERE id=$1', [req.params.id])
+    .then(() => res.sendStatus(204))
+    .catch(console.error);
 });
 
 app.get('*', (req,res) => res.redirect(CLIENT_URL));
